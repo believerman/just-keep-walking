2c30e4bb53f75731e4fdf3f7fcdb3c3309fafdf3
jtmcgrath <joe@jmcgrath.co.uk>
2017-05-20 15:25:12 +0300

    Added more comprehensive test data.
    Since the intention is for this to be used daily, it makes sense to test
    with a much larger dataset than would be possible with manual entry.

--- app/utils/db.js
+++ app/utils/db.js
@@ -3,23 +3,41 @@ import Dexie from "dexie";
 const db = new Dexie("JustKeepWalking");
 db.version(1).stores({ walks: "++id" });
 
-/* *
+/* */
 db.on("populate", function() {
-  let testData = [
-    {id: 1, start: new Date(2016, 11, 15, 12), end: new Date(2016, 11, 15, 13, 45)},
-    {id: 2, start: new Date(2016, 11, 16, 12), end: new Date(2016, 11, 16, 12, 45)},
-    {id: 3, start: new Date(2016, 11, 17, 12), end: new Date(2016, 11, 17, 14)},
-    {id: 4, start: new Date(2017, 1, 18, 12), end: new Date(2017, 1, 18, 13, 15)},
-    {id: 5, start: new Date(2017, 1, 19, 12), end: new Date(2017, 1, 19, 13, 10)},
-    {id: 6, start: new Date(2017, 1, 20, 12), end: new Date(2017, 1, 20, 13)},
-    {id: 7, start: new Date(2017, 2, 21, 12), end: new Date(2017, 2, 21, 12, 45)},
-    {id: 8, start: new Date(2017, 2, 22, 12), end: new Date(2017, 2, 22, 13, 10)},
-    {id: 9, start: new Date(2017, 2, 23, 12), end: new Date(2017, 2, 23, 12, 25)},
-    {id: 10, start: new Date(2017, 3, 24, 12), end: new Date(2017, 3, 24, 12, 55)},
-    {id: 11, start: new Date(2017, 3, 25, 12), end: new Date(2017, 3, 25, 12, 30)},
-    {id: 12, start: new Date(2017, 3, 26, 12), end: new Date(2017, 3, 26, 12, 45)}
-  ];
-  testData.forEach(item => db.walks.add(item));
+  const range = (a, b) => {
+    let range = [];
+    for (let i = a; i <= b; i++) {
+      range.push(i);
+    }
+    return range;
+  };
+
+  const years = [2016, 2017];
+  const months = range(0, 11);
+  const days = range(1,30);
+
+  let i = 1;
+  years.forEach(year => {
+    months.forEach(month => {
+      days.forEach(day => {
+        let start = Math.floor(Math.random() * 10 + 10);
+        let duration = Math.random() + 0.5;
+        let endHours = start + duration;
+        let end = {
+          hours: Math.floor(endHours),
+          minutes: endHours * 60 % 60
+        };
+
+        db.walks.add({
+          id: i,
+          start: new Date(year, month, day, start),
+          end: new Date(year, month, day, end.hours, end.minutes)
+        });
+        i++;
+      });
+    });
+  });
 });
 /* */
 
